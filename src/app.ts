3412d53d8369de9a7c81ac959542d722afb308cd
Govind <113257576+govindKulk@users.noreply.github.com>
2025-03-10 03:51:53 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,14 @@ import { BoardRoom } from "./GameRoom";
 import cors from 'cors'
 const app = express();
 
+
+app.use(cors({
+  origin: ["https://boardstorm.vercel.app/", "http://localhost:3000/"],
+  methods: ["GET", "POST"],
+  credentials: true
+
+}))
+
 // Middleware
 app.use(express.json());
 
@@ -13,12 +21,7 @@ app.get("/", (req, res) => {
 
 const boardRoom = BoardRoom.getInstance();
 
-app.use(cors({
-  origin: ["https://boardstorm.vercel.app/", "http://localhost:3000/"],
-  methods: ["GET", "POST"],
-  credentials: true
 
-}))
 
 app.get('/rooms', (req,res) => {
   const rooms = boardRoom.getRooms();
