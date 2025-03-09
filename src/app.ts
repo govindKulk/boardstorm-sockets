2ca0bdcecb6a070449d74347a787ca4833600501
Govind <113257576+govindKulk@users.noreply.github.com>
2025-03-10 04:10:28 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ["https://boardstorm.vercel.app/", "http://localhost:3000/"],
+  origin: ["https://boardstorm.vercel.app", "http://localhost:3000"],
   methods: ["GET", "POST"],
   credentials: true
 
